2c8092c68ff6746ef292cc642ac94ca3ec536ff0
Matt0911 <Mattmanhardt@Gmail.com>
2017-03-15 15:20:40 -0600

    email

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
+var nodemailer = require('nodemailer');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+app.use('/sayHello', router);
+router.post('/', handleSayHello); // handle the route at yourdomain.com/sayHello
+
+function handleSayHello(req, res) {
+    // Not the movie transporter!
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'creative4email@gmail.com', // Your email id
+            pass: 'creative4' // Your password
+        }
+    });
+
+    var text = 'Hello from \n\n';
+
+    var mailOptions = {
+      from: 'mattmanhardt@gmail.com>', // sender address
+      to: 'matt0911@byu.edu', // list of receivers
+      subject: 'Email Example', // Subject line
+      text: text //, // plaintext body
+      // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+      if(error){
+          console.log(error);
+          res.json({yo: 'error'});
+      }else{
+          console.log('Message sent: ' + info.response);
+          res.json({yo: info.response});
+      };
+    });
+}
 
 module.exports = router;
